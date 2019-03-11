141f6cdcac08ea086d60d1c31012a5549aa73b3f
meowcodes <meowcodes@Sams-iPhone.localdomain>
2019-03-11 12:02:49 -0700

    create register, authenticate functions in user models and create login route

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 /** User class for message.ly */
-
-
+const bcrypt = require("bcrypt");
+const db = require("../db");
+const { SECRET_KEY, BCRYPT_WORK_ROUNDS } = require("../config")
 
 /** User of the site. */
 
@@ -10,15 +11,43 @@ class User {
    *    {username, password, first_name, last_name, phone}
    */
 
-  static async register({username, password, first_name, last_name, phone}) { }
+  static async register({username, password, first_name, last_name, phone}) {
+    // hash password
+    const hashedPW = await bcrypt.hash(password, BCRYPT_WORK_ROUNDS);
+
+    // insert into database
+    const result = await db.query(`
+      INSERT INTO users (username, password, first_name, last_name, phone)
+        VALUES ($1, $2, $3, $4, $5)
+        RETURNING username, password, first_name, last_name, phone`, 
+      [username, hashedPW, first_name, last_name, phone]);
+
+    // return {username, hashedpassword, first_name, last_name, phone}
+    return result.rows[0];
+  }
 
   /** Authenticate: is this username/password valid? Returns boolean. */
 
-  static async authenticate(username, password) { }
+  static async authenticate(username, password) {
+    // check if username exists
+    const result = await db.query(`
+      SELECT password FROM users
+        WHERE username = $1`,
+      [username]
+    );
+
+    // get hashed password
+    const hashedPW = result.rows[0].password;
+
+    // return boolean
+    return await bcrypt.compare(password, hashedPW);
+  }
 
   /** Update last_login_at for user */
 
-  static async updateLoginTimestamp(username) { }
+  static async updateLoginTimestamp(username) { 
+
+  }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,37 @@
+const express = require("express");
+const jwt = require("jsonwebtoken")
+
+const User = require("./models/user");
+
+const router = new express.Router();
+
+const { SECRET_KEY, BCRYPT_WORK_ROUNDS } = require("../config")
+const OPTIONS = { expiresIn: 60 * 60 }
+
+
 /** POST /login - login: {username, password} => {token}
  *
  * Make sure to update their last-login!
  *
  **/
+router.post("/login", async function(req, res, next) {
+    try{
+        // get the username. & password
+        const { username, password } = req.body;
+    
+        // authenticate
+        if(await User.authenticate(username, password)){
+        // create a token
+            const token = jwt.sign({ username }, SECRET_KEY, OPTIONS);
+            // send token
+            return res.json({ token });
+        }
+
+        throw new expressError("Invalid credentials", 400)
+    } catch(err) {
+        return next(err);
+    }
+})
 
 
 /** POST /register - register user: registers, logs in, and returns token.
