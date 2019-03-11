de50a4992e0fd85fccb7e54db816cb4e3ae4b8f2
meowcodes <meowcodes@Sams-iPhone.localdomain>
2019-03-11 11:31:27 -0700

    add .env

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 /** Common config for message.ly */
 
 // read .env files and make environmental variables
-
+// convention for reading file named ".env" (where SECRET_KEY comes from)
 require("dotenv").config();
 
-const SECRET_KEY = process.env.SECRET_KEY || "secret";
+const SECRET_KEY = process.env.SECRET_KEY;
 const BCRYPT_WORK_ROUNDS = 10;
 
 
